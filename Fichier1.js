ca8219ed82d1b38ec37f985c84ed9bd95b6ac4bb
couscoudet <ysoultane@gmail.com>
2019-10-24 00:21:24 +0200

    test alerte

--- Fichier1.js
+++ Fichier1.js
@@ -1,2 +1,7 @@
-len prenom = "yannick";
+let prenom = "yannick";
 console.log(prenom);
+
+//créer un tableau : array
+let episodes = ["starlion découvre la planete X","la revanche de Mumvra","L'épée du pouvoir","la vision par delà la vision"];
+alerte = episodes[2];
+alert(alerte);
