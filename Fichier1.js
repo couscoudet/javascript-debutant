e01682d65dfd3b4dffc75c32ab143f5ac3e21231
couscoudet <ysoultane@gmail.com>
2019-11-07 23:06:23 +0100

    Fichier1.js

--- Fichier1.js
+++ Fichier1.js
@@ -5,3 +5,33 @@ console.log(prenom);
 let episodes = ["starlion découvre la planete X","la revanche de Mumvra","L'épée du pouvoir","la vision par delà la vision"];
 alerte = episodes[2];
 alert(alerte);
+
+//exercice sur les boucles for 
+
+const numberOfPassengers = 10;
+
+for (let i = 0; i < numberOfPassengers; i++){
+    console.log("Passager embarqué!")
+}
+//première commande : démarrage de la boucle
+//deuxième commande : instruction de fin de boucle : sort de la boucle lorsqu'elle est false
+//troiseme commande : incrementation
+
+
+//pour les odres, précis, tableaux : for...of et for...in
+
+const passengers = [
+    "John Mclain",
+    "Sylvester Stallone",
+    "Carole Mamour",
+    "Jean Claude Van Damme",
+    "Charlize Theron",
+]
+
+for (let i in passengers) {
+    console.log("Embarquement immédiat du passager" + passengers[i]);
+}
+
+for (let passenger of passengers){
+    console.log("Passager " + passenger + " embarqué");
+}
